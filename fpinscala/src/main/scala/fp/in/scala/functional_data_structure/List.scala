84efe478b72b41d79f1b71f00a7edab5f963ec05
xiaojun.ren <xiaojun.z.ren@tmr.qld.gov.au>
2022-03-30 21:22:25 +1100

    added drop and dropWhile for List

--- fpinscala/src/main/scala/fp/in/scala/functional_data_structure/List.scala
+++ fpinscala/src/main/scala/fp/in/scala/functional_data_structure/List.scala
@@ -1,5 +1,7 @@
 package fp.in.scala.functional_data_structure
 
+import scala.annotation.tailrec
+
 enum List[+A]:
   case Nil
   case Cons(head: A, tail: List[A])
@@ -18,4 +20,16 @@ object List:
     def tail = as match {
       case Nil => Nil
       case Cons(head, tail) => tail
+    }
+
+    def drop(n: Int): List[A] =
+      if n <= 0 then as
+      else as match {
+        case Nil => Nil
+        case Cons(_, tail) => tail.drop(n - 1)
+      }
+
+    def dropWhile(p: A => Boolean): List[A] = as match {
+      case Cons(head, tail) if p(head) => tail.dropWhile(p)
+      case _ => as
     }
--- fpinscala/src/test/scala/fp/in/scala/functional_data_structure/ListTest.scala
+++ fpinscala/src/test/scala/fp/in/scala/functional_data_structure/ListTest.scala
@@ -3,12 +3,22 @@ package fp.in.scala.functional_data_structure
 import munit.FunSuite
 
 class ListTest extends FunSuite {
-  val list = List(1, 2, 3)
+  private val list = List(1, 2, 3)
   test("head") {
     assert(list.head == 1)
   }
 
   test("tail") {
     assert(list.tail == List(2, 3))
+  }
+
+  test("drop") {
+    assert(list.drop(2) == List(3))
+    assert(list.drop(1) == List(2, 3))
+  }
+
+  test("dropWhile") {
+    assert(list.dropWhile(_ % 2 != 0) == List(2, 3))
+    assert(list.dropWhile(_ <= 2) == List(3))
   }
 }

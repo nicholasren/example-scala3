878a8c9e41092be5c2b44f5eebbcc3ea50f5ab6f
xiaojun.ren <xiaojun.z.ren@tmr.qld.gov.au>
2022-04-05 20:48:27 +1000

    added example for  functional state

---
+++ fpinscala/src/main/scala/fp/in/scala/functional_state/Rand.scala
@@ -0,0 +1,65 @@
+package fp.in.scala.functional_state
+
+import scala.annotation.tailrec
+
+trait RandGenerator:
+  def nextInt: (Int, RandGenerator)
+
+/**
+ * a randomly generated '''A'''
+ *
+ * @tparam A
+ */
+type Rand[+A] = RandGenerator => (A, RandGenerator)
+
+object Rand:
+  def unit[A](a: A): Rand[A] = (rng: RandGenerator) => (a, rng)
+
+  def map[A, B](state: Rand[A])(f: A => B): Rand[B] =
+    (rng: RandGenerator) =>
+      // generate a new 'a' and a new `RandGenerator`
+      val (a, newGenerator) = state(rng)
+      // apply 'f' against newly generated 'a' which result to a 'b'
+      val b = f(a)
+      // return a tuple
+      (b, newGenerator)
+
+
+case class SimpleRandGenerator(seed: Long) extends RandGenerator :
+  override def nextInt: (Int, RandGenerator) =
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRandGenerator(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+
+object SimpleRandGenerator:
+
+  def nonNegativeInt: Rand[Int] =
+    @tailrec
+    def go(rng: RandGenerator): (Int, RandGenerator) =
+      val next = rng.nextInt
+      if (next._1 > 0)
+        next
+      else
+        go(next._2)
+
+    rng => go(rng)
+
+  /**
+   * Generate ''count'' number of Int with provided initial ''RNG''
+   *
+   * @param count number of required ints
+   * @return Rand[List[Int]]
+   */
+  def ints(count: Int): Rand[List[Int]] =
+    @tailrec
+    def go(result: List[Int])(count: Int)(rng: RandGenerator): (List[Int], RandGenerator) =
+      if (count == 0)
+        (result, rng)
+      else
+        val next = rng.nextInt
+        go(next._1 :: result)(count - 1)(next._2)
+
+    rng => go(List())(count)(rng)
+
+  def nonNegativeEven: Rand[Int] = Rand.map(nonNegativeInt)(i => i - i % 2)
---
+++ fpinscala/src/test/scala/fp/in/scala/functional_state/RandTest.scala
@@ -0,0 +1,30 @@
+package fp.in.scala.functional_state
+
+import fp.in.scala.functional_state.SimpleRandGenerator.{ints, nonNegativeEven, nonNegativeInt}
+
+class RandTest extends munit.FunSuite {
+
+  private val rng: RandGenerator = SimpleRandGenerator(42)
+
+  test("nextInt") {
+    assert(16159453 == rng.nextInt._1)
+  }
+
+  test("nonNegativeInt") {
+    (0 until 100).foreach { _ =>
+      assert(nonNegativeInt(rng)._1 > 0)
+    }
+  }
+
+  test("nonNegativeEven") {
+    (0 until 100).foreach { _ =>
+      assert(nonNegativeEven(rng)._1 % 2 == 0)
+    }
+  }
+
+  test("ints") {
+    val xs = ints(100)(rng)
+
+    assert(xs._1.length == 100)
+  }
+}
